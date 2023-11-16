c7bf0acdbcf28cddc8359e61971d1b6f8f90d941
Silvana A <alfonsosilvana16@gmail.com>
2023-11-16 14:27:52 -0300

    section catalogo

---
+++ catalogo/catalogo.js
@@ -0,0 +1,124 @@
+const productos = [
+    {
+      id: 1,
+      image: "",
+      title: "Producto 1",
+      price: 19.99,
+      description: "Descripción del Producto 1",
+      size: ""
+    },
+    {
+      id: 2,
+      image: "",
+      title: "Producto 2",
+      price: 29.99,
+      description: "Descripción del Producto 2",
+      size: ""
+    },
+    {
+      id: 3,
+      image: "",
+      title: "Producto 3",
+      price: 39.99,
+      description: "Descripción del Producto 3",
+      size: ""
+    },
+    {
+      id: 4,
+      image: "",
+      title: "Producto 4",
+      price: 49.99,
+      description: "Descripción del Producto 4",
+      size: ""
+    },
+    {
+      id: 5,
+      image: "",
+      title: "Producto 5",
+      price: 59.99,
+      description: "Descripción del Producto 5",
+      size: ""
+    },
+    {
+      id: 6,
+      image: "",
+      title: "Producto 6",
+      price: 69.99,
+      description: "Descripción del Producto 6",
+      size: ""
+    },
+    {
+      id: 7,
+      image: "",
+      title: "Producto 7",
+      price: 79.99,
+      description: "Descripción del Producto 7",
+      size: ""
+    },
+    {
+      id: 8,
+      image: "",
+      title: "Producto 8",
+      price: 89.99,
+      description: "Descripción del Producto 8",
+      size: ""
+    },
+    {
+      id: 9,
+      image: "",
+      title: "Producto 9",
+      price: 99.99,
+      description: "Descripción del Producto 9"
+    },
+    {
+      id: 10,
+      image: "",
+      title: "Producto 10",
+      price: 109.99,
+      description: "Descripción del Producto 10",
+      size: ""
+    },
+    {
+      id: 11,
+      image: "",
+      title: "Producto 11",
+      price: 119.99,
+      description: "Descripción del Producto 11",
+      size: ""
+    },
+    {
+      id: 12,
+      image: "",
+      title: "Producto 12",
+      price: 129.99,
+      description: "Descripción del Producto 12",
+      size: ""
+    },
+    {
+      id: 13,
+      image: "",
+      title: "Producto 13",
+      price: 139.99,
+      description: "Descripción del Producto 13",
+      size: ""
+    },
+    {
+      id: 14,
+      image: "",
+      title: "Producto 14",
+      price: 149.99,
+      description: "Descripción del Producto 14",
+      size: ""
+    },
+    {
+      id: 15,
+      image: "",
+      title: "Producto 15",
+      price: 159.99,
+      description: "Descripción del Producto 15",
+      size: ""
+    }
+];
+
+console.log(productos);
+  
